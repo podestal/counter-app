03b5d042a82377e60bb6f2e28b1215bb01162847
podestal <luisrodriguezpodesta@gmail.com>
2023-12-09 09:31:56 -0500

    visibility toggle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const VisibilityToggle = () => {
+
+  let [visibility, setVisibility] = React.useState(false)
+
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+  }
+
+  return (
+    <div>
+      <h1>Visibility Toggle</h1>
+      <button onClick={toggleVisibility}>{visibility ? "Hide details" : "Show details"}</button>
+      {visibility && <p>Hey, these are some details you can see now</p>}
+    </div>
+  )
+}
+
 const Counter = () => {
 
   let [count, setCount] = React.useState(0)
@@ -32,7 +49,8 @@ const Counter = () => {
 function App() {
   return (
     <div>
-      <Counter />
+      {/* <Counter /> */}
+      <VisibilityToggle />
     </div>
   );
 }
