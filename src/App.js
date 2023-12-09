116931e7c75fa3026a52c9cf1985eb2eb5e8e8b8
podestal <luisrodriguezpodesta@gmail.com>
2023-12-09 09:22:15 -0500

    counter app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const Counter = () => {
+
+  let [count, setCount] = React.useState(0)
+
+  const handleAddOne = () => {
+    setCount(count += 1)
+  }
+
+  const handleMinusOne = () => {
+    setCount(count -= 1)
+  }
+
+  const handleReset = () => {
+    setCount(0)
+  }
+
+  return (
+    <div>
+      <h1>Count</h1>
+      <p>{count}</p>
+      <button onClick={handleAddOne}>+1</button>
+      <button onClick={handleMinusOne}>-1</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      
+      <Counter />
     </div>
   );
 }
